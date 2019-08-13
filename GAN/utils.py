60571c08d703796df7006f0a8d166a459d919414
JJH <gokae@naver.com>
2019-08-13 19:09:58 +0900

    add

--- GAN/utils.py
+++ GAN/utils.py
@@ -5,10 +5,31 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torch.utils as utils
 import torchvision.utils as vutils
-
+import torchvision
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def dataloader(data = 'mnist', root = '/data', train = True, download = True, size = 32, transform = True, batch_size = 128, shuffle = True, num_workers = 2):
+    if transform:
+        _transform = transforms.Compose([
+                                                transforms.Resize(size,size),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    else:
+        _transform = transform
+    
+    if data == 'mnist':
+        trainset = torchvision.datasets.MNIST(root= '/data', train=True, transform=_transform, download=True)
+    elif data == 'cifar10':
+        trainset = torchvision.datasets.CIFAR10(root= '/data', train=True, transform=_transform, download=True)
+
+
+    
+    
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size = batch_size, shuffle = shuffle, num_workers = num_workers)
+    return trainloader
 
 
 def make_weights_function(mean = 0 , sd = 0.02):
